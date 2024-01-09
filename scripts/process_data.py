bb1cb569579539fc2827e4c3e976922f9c3b2973
Alexandr Medovikov <medovikov-aleksandr66@mail.ru>
2024-01-09 21:18:10 +0500

    update process_data

--- scripts/process_data.py
+++ scripts/process_data.py
@@ -4,7 +4,7 @@ df = pd.read_csv('/home/ml-srv-admin/xflow_project/datasets/data.csv')
 
 for i in range(len(df.id)):
   max = df.counts.max()
-  min =df.counts.min()
+  min = df.counts.min()
   df.counts[i] = (df.counts[i] - min)/(max - min)
 
 df.to_csv('/home/ml-srv-admin/xflow_project/datasets/data_processed.csv', index=False)
