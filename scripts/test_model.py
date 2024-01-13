444d55dfa9c2b1af01bd5cab17c72a11f844efc5
Alexandr Medovikov <medovikov-aleksandr66@mail.ru>
2024-01-13 14:20:05 +0500

    update scripts

--- scripts/test_model.py
+++ scripts/test_model.py
@@ -1,12 +1,12 @@
 from sklearn.metrics import mean_squared_error
 import pickle
 import pandas as pd
-from catboost import CatBoostRegressor, Pool
+from catboost import Pool
 
 df_test = pd.read_csv('/home/ml-srv-admin/xflow_project/datasets/data_test.csv')
 
 test_pool = Pool(data=df_test.id, cat_features=['id'], feature_names=['id'])
-model = CatBoostRegressor()
+
 with open('/home/ml-srv-admin/xflow_project/models/model.pkl', 'rb') as f:
     model = pickle.load(f)
 
